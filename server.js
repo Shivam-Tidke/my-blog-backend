9e7a8cee829f9b21bc75737915e01792bf7a8974
Shivam-Tidke <shivamtidke2000@gmail.com>
2025-06-03 03:26:20 +0530

    install some libraries, create a blogDB database and connect using mongoose

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors"
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+const app = express()
+connectDB();
+
+app.use (cors());
+app.use (express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`));
+
